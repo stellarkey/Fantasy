8d25b904952a5ba0794f07db623cfc0519eedd2c
Vel <velocfc@gmail.com>
2020-01-21 05:54:25 +0800

    install help

--- necessary.py
+++ necessary.py
@@ -12,3 +12,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import networkx as nx
+
+def main():
+    EH = EnvironmentalHelper()
+    EH.install_requirements()
+
+class EnvironmentalHelper():
+    def install_requirements(self):
+        import os
+        os.system('pip3 install chardet')
+        os.system('pip3 install numpy')
+        os.system('pip3 install pandas')
+        os.system('pip3 install matplotlib')
+        os.system('pip3 install seaborn')
+        os.system('pip3 install PyQt5')
+        os.system('pip3 install networkx')
+
+if __name__ == '__main__':
+    main()
+    
